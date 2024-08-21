8555e201c5a9cd69450b35b52decf9caafeb8a05
MohammedHussain <68030846+Moh9998@users.noreply.github.com>
2024-08-21 14:04:29 +0300

    Adding Test Clases

---
+++ API_ Clean_Template.Domain/Enums/TestEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API__Clean_Template.Domain.Enums
+{
+    internal class TestEnum
+    {
+    }
+}
